083141dd3eb976b8b93f7d850bc600e4669e7670
vaasuu <38670296+vaasuu@users.noreply.github.com>
2021-12-05 02:55:16 +0200

    2021 day 3 part 2

--- 2021/day_03/code.py
+++ 2021/day_03/code.py
@@ -38,16 +38,47 @@ def which_to_keep(avg, return_val_if_equal):
         most_common = 1
     else:
         return return_val_if_equal
-    return most_common
+
+    if return_val_if_equal == 1:
+        return most_common
+    else:
+        return int(not most_common)
+
+
+def calc_keep_bits(a, return_val_if_equal):
+    width = len(a[0])
+    columns = [column(a, col) for col in range(width)]
+    column_averages = [average(col) for col in columns]
+    keep_bits = [which_to_keep(col, return_val_if_equal) for col in column_averages]
+    return keep_bits
+
+
+def calc_number_to_keep(a, return_val_if_equal):
+
+    numbers_array = deepcopy(a)
+    width = len(numbers_array[0])
+
+    for col in range(width):
+        keep_bits = calc_keep_bits(numbers_array, return_val_if_equal)
+        for row in a:
+            if row[col] != keep_bits[col]:
+                try:
+                    numbers_array.remove(row)
+                except:
+                    pass
+            if len(numbers_array) == 1:
+                num_str = "".join([str(x) for x in numbers_array[0]])
+                num = int(num_str, 2)
+                return num
 
 
 def part2(lines):
-    width = len(lines[0])
     a = [list(map(int, line)) for line in lines]
 
-    columns = [column(a, col) for col in range(width)]
-    column_averages = [average(col) for col in columns]
-    keep_bits = [which_to_keep(col, 1) for col in column_averages]
+    oxygen_generator_rating = calc_number_to_keep(a, 1)
+    CO2_scrubber_rating = calc_number_to_keep(a, 0)
+    ans = oxygen_generator_rating * CO2_scrubber_rating
+    return ans
 
 
 def main():
