f7ec2dc9792b7ee1ce7cb4bc66888e1a2d7556b3
vaasuu <38670296+vaasuu@users.noreply.github.com>
2021-12-08 05:12:20 +0200

    2021 day 6 reworked part 2 and part 1

--- 2021/day_06/code.py
+++ 2021/day_06/code.py
@@ -1,45 +1,21 @@
-DEBUG = False
-DEBUG2 = True
-
-
 def read_input(filename):
     with open(filename) as file:
         input = [int(num) for num in file.read().strip().split(",")]
     return input
 
 
-def list_to_str(lst):
-    lst_str = ", ".join(map(str, lst))
-    return lst_str
+def calculate_fish(fish_states, days):
+    fish_state_count = [0 for i in range(9)]
 
+    for state in fish_states:
+        fish_state_count[state] += 1
 
-def part1(timers, days):
-    DEBUG and print(f"Initial state: \t{list_to_str(timers)}")
     for i in range(days):
-        for index in range(len(timers)):
-            timers[index] -= 1
-            if timers[index] < 0:
-                timers[index] = 6
-                timers.append(8)
-        DEBUG and print(f"After \t{i+1}\t days: {list_to_str(timers)}")
-    total_fish = len(timers)
-    print(f"After {days} days, there are {total_fish} fish")
-    return total_fish
+        births = fish_state_count.pop(0)
+        fish_state_count.append(births)
+        fish_state_count[6] += births
 
-
-def part2(timers, days):
-    DEBUG and print(f"Initial state: \t{list_to_str(timers)}")
-    for i in range(days):
-        for index in range(len(timers)):
-            timers[index] -= 1
-            if timers[index] < 0:
-                timers[index] = 6
-                timers.append(8)
-
-        DEBUG and print(f"After \t{i+1}\t days: {list_to_str(timers)}")
-        DEBUG2 and print(f"After \t{i+1}\t days: fish count is {len(timers)}")
-    total_fish = len(timers)
-    print(f"After {days} days, there are {total_fish} fish")
+    total_fish = sum(fish_state_count)
     return total_fish
 
 
@@ -47,15 +23,15 @@ def main():
     sample_input = read_input("sample_input.txt")
     input = read_input("input.txt")
 
-    assert part1(sample_input, 18) == 26
-    # assert part1(sample_input, 80) == 5934
+    assert calculate_fish(sample_input, 18) == 26
+    assert calculate_fish(sample_input, 80) == 5934
 
-    part1ans = part1(input, 80)
+    part1ans = calculate_fish(input, 80)
     print("part1:", part1ans)
 
-    assert part2(sample_input, 256) == 26984457539
+    assert calculate_fish(sample_input, 256) == 26984457539
 
-    part2ans = part2(input, 256)
+    part2ans = calculate_fish(input, 256)
     print("part2:", part2ans)
 
 
