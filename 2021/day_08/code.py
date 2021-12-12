e7a3041bb79ca31f736d68618aefaf8fe0012134
vaasuu <38670296+vaasuu@users.noreply.github.com>
2021-12-12 05:34:53 +0200

    2021 day 8 part 2

--- 2021/day_08/code.py
+++ 2021/day_08/code.py
@@ -21,6 +21,68 @@ def part1(input):
     return found_digit_count
 
 
+def part2(input):
+    output_numbers = []
+
+    for line in input:
+        digit_sets_dict = {}
+        list_235 = []
+        list_069 = []
+
+        unique_signal_patterns, output_values_string = line.split(" | ")
+        for digit_str in unique_signal_patterns.split(" "):
+            digit_length = len(digit_str)
+            easy_digits = {2: 1, 4: 4, 3: 7, 7: 8}  # digit_segment_count:digit_int
+            if digit_length in easy_digits.keys():
+                digit = easy_digits[digit_length]
+                digit_sets_dict[digit] = set(digit_str)
+            elif digit_length == 5:
+                list_235.append(set(digit_str))
+            elif digit_length == 6:
+                list_069.append(set(digit_str))
+
+        for digit_set in list_235:
+            if len(digit_set - digit_sets_dict[1]) == 3:
+                digit_sets_dict[3] = digit_set
+                list_235.remove(digit_set)
+        for digit_set in list_235:
+            if len(digit_set - digit_sets_dict[4]) == 3:
+                digit_sets_dict[2] = digit_set
+                list_235.remove(digit_set)
+                digit_sets_dict[5] = list_235[0]
+            else:
+                digit_sets_dict[5] = digit_set
+                list_235.remove(digit_set)
+                digit_sets_dict[2] = list_235[0]
+
+        for digit_set in list_069:
+            if len(digit_set - digit_sets_dict[1]) == 5:
+                digit_sets_dict[6] = digit_set
+                list_069.remove(digit_set)
+        for digit_set in list_069:
+            if len(digit_set - digit_sets_dict[4]) == 3:
+                digit_sets_dict[0] = digit_set
+                list_069.remove(digit_set)
+                digit_sets_dict[9] = list_069[0]
+            else:
+                digit_sets_dict[9] = digit_set
+                list_069.remove(digit_set)
+                digit_sets_dict[0] = list_069[0]
+
+        # invert dict so key is sorted segment letters and value is integer
+        ivd = {"".join(sorted(v)): k for k, v in digit_sets_dict.items()}
+        output_digits_int_list = [
+            ivd["".join(sorted(digit_str))]
+            for digit_str in output_values_string.split(" ")
+        ]
+        output_digits_str_list = [str(i) for i in output_digits_int_list]
+        output_digits_str = "".join(output_digits_str_list)
+        output_number = int(output_digits_str)
+        output_numbers.append(output_number)
+    ans = sum(output_numbers)
+    return ans
+
+
 def main():
     sample_input = read_input("sample_input.txt")
     input = read_input("input.txt")
@@ -30,10 +92,10 @@ def main():
     part1ans = part1(input)
     print("part1:", part1ans)
 
-    # assert part2(sample_input) == 61229
+    assert part2(sample_input) == 61229
 
-    # part2ans = part2(input)
-    # print("part2:", part2ans)
+    part2ans = part2(input)
+    print("part2:", part2ans)
 
 
 if __name__ == "__main__":
