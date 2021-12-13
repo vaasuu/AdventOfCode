ae3bd3b7d7c24bb56319a790a699f81f7b5b055f
vaasuu <38670296+vaasuu@users.noreply.github.com>
2021-12-13 04:15:43 +0200

    2021 day 10 part 1

--- 2021/day_10/code.py
+++ 2021/day_10/code.py
@@ -1,11 +1,44 @@
+from collections import deque
+
+
 def read_input(filename):
     with open(filename) as file:
         input = [line.strip() for line in file.readlines()]
     return input
 
 
+char_groups = [("(", ")"), ("[", "]"), ("{", "}"), ("<", ">")]
+start_chars = [char for (char, _) in char_groups]
+end_chars = [char for (_, char) in char_groups]
+char_pairs = {
+    **{start: end for (start, end) in char_groups},
+    **{end: start for (start, end) in char_groups},
+}  # merge two dicts together https://stackoverflow.com/a/26853961
+
+
+def check_line(line):
+    char_stack = deque()
+    for char in line:
+        if char in start_chars:
+            char_stack.append(char)
+        else:
+            start_char = char_stack.pop()
+            if char_pairs[start_char] != char:
+                return ("corrupt", char)
+    return ("valid", None)
+
+
 def part1(input):
-    pass
+    score_table = {")": 3, "]": 57, "}": 1197, ">": 25137}
+    scores = []
+
+    for line in input:
+        state, char = check_line(line)
+        if state == "corrupt":
+            scores.append(score_table[char])
+
+    ans = sum(scores)
+    return ans
 
 
 def main():
@@ -17,7 +50,7 @@ def main():
     part1ans = part1(input)
     print("part1:", part1ans)
 
-    # assert part2(sample_input) == 
+    # assert part2(sample_input) ==
 
     # part2ans = part2(input)
     # print("part2:", part2ans)
