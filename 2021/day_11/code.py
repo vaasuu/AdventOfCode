690a1246754f3b61f5f27c5db4823a7fb988df70
vaasuu <38670296+vaasuu@users.noreply.github.com>
2021-12-16 05:52:24 +0200

    2021 day 11 part 1 WIP WTF

--- 2021/day_11/code.py
+++ 2021/day_11/code.py
@@ -27,9 +27,59 @@ def get_adjacent_location_indices(i, j, hight, width):
     return adjacent_indices
 
 
+def print_matrix(matrix):
+    s = [[str(e) for e in row] for row in matrix]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = ' '.join('{{:{}}}'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    print('\n'.join(table))
+    print("\n")
+
+
+def step_forward(matrix, n):
+    width = len(matrix[0])
+    height = len(matrix)
+
+    has_flashed = set()
+    print_matrix(matrix)
+
+    def flash_adjacent(i, j, height, width):
+        adjacent_indices = set(
+            get_adjacent_location_indices(i, j, height, width)
+        )
+        for adj_i, adj_j in adjacent_indices:
+            matrix[adj_i][adj_j] += 1
+            if matrix[adj_i][adj_j] > 9 and (adj_i, adj_j) not in has_flashed:
+                has_flashed.add((adj_i, adj_j))
+                print_matrix(matrix)
+                flash_adjacent(adj_i, adj_j, height, width)
+        return 
+
+
+    for i in range(height):
+        for j in range(width):
+            matrix[i][j] += 1
+    print_matrix(matrix)
+
+    for i in range(height):
+        for j in range(width):
+            if matrix[i][j] > 9:
+                has_flashed.add((i, j))
+                print_matrix(matrix)
+                flash_adjacent(i, j, height, width)
+
+    for i in range(height):
+        for j in range(width):
+            if matrix[i][j] in has_flashed:
+                matrix[i][j] = 0
+    
+    print_matrix(matrix)
+    print("end")
+
+
 def part1(lines):
     matrix = [list(map(int, line)) for line in lines]
-
+    step_forward(matrix, n=1)
 
 def main():
     sample_lines = read_input("sample_input.txt")
