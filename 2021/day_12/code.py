97b6bb06c5b61bfd782053f991efa6d9d3824e31
vaasuu <38670296+vaasuu@users.noreply.github.com>
2021-12-21 18:56:52 +0200

    2021 day 12 part 1

--- 2021/day_12/code.py
+++ 2021/day_12/code.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def read_input(filename):
     with open(filename) as file:
         lines = [line.strip() for line in file.readlines()]
@@ -6,6 +9,26 @@ def read_input(filename):
 
 def part1(lines):
 
+    edges = defaultdict(set)
+    for line in lines:
+        source, destination = line.split("-")
+        edges[source].add(destination)
+        edges[destination].add(source)
+
+    all_paths = set()
+    todo = [("start",)]
+    while todo:
+        path = todo.pop()
+
+        if path[-1] == "end":
+            all_paths.add(path)
+            continue
+
+        for candidate_path in edges[path[-1]]:
+            if not candidate_path.islower() or candidate_path not in path:
+                todo.append(path + (candidate_path,))
+
+    return len(all_paths)
 
 
 def main():
