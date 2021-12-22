0dccfaf35527ece38d9cef9f7183d8b637b1ca08
vaasuu <38670296+vaasuu@users.noreply.github.com>
2021-12-22 09:48:19 +0200

    2021 day 12 part 2

--- 2021/day_12/code.py
+++ 2021/day_12/code.py
@@ -31,6 +31,41 @@ def part1(lines):
     return len(all_paths)
 
 
+def double_small_visit(path):
+    lower = [vertex for vertex in path if vertex.islower()]
+    is_small_double_visit = len(lower) != len(set(lower))
+    return is_small_double_visit
+
+
+def part2(lines):
+
+    edges = defaultdict(set)
+    for line in lines:
+        source, destination = line.split("-")
+        edges[source].add(destination)
+        edges[destination].add(source)
+
+    all_paths = set()
+    todo = [("start",)]
+
+    while todo:
+        path = todo.pop()
+
+        if path[-1] == "end":
+            all_paths.add(path)
+            continue
+
+        for candidate_path in edges[path[-1]]:
+            if candidate_path == "start":
+                continue
+            elif candidate_path.isupper() or candidate_path not in path:
+                todo.append(path + (candidate_path,))
+            elif not double_small_visit(path):
+                todo.append(path + (candidate_path,))
+
+    return len(all_paths)
+
+
 def main():
     sample_lines = read_input("sample_input.txt")
     lines = read_input("input.txt")
@@ -40,10 +75,10 @@ def main():
     part1ans = part1(lines)
     print("part1:", part1ans)
 
-    # assert part2(sample_lines) ==
+    assert part2(sample_lines) == 36
 
-    # part2ans = part2(lines)
-    # print("part2:", part2ans)
+    part2ans = part2(lines)
+    print("part2:", part2ans)
 
 
 if __name__ == "__main__":
